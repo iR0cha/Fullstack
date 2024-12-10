bc5501088d07209298641cf44677b4db3958bfbc
Arquero <PC@arch.linux>
2024-12-09 23:48:31 -0300

    fix: CreateProduct ahora devuelve un div

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //Importar nuestros componentes
 import ShowProducts from './Components/ShowProducts';
 import CreateProduct from './Components/CreateProduct';
-import EditProduct from './Components/CreateProduct';
+import EditProduct from './Components/EditProduct';
 
 function App() {
-  return (
-      <div className='App'>
-          <BrowserRouter>
-              <Routes>
-                  <Route path='/' element={<ShowProducts/>}/>
-                  <Route path='/create' element={<CreateProduct/>} />
-                  <Route path='/edit/:id' element={<EditProduct/>} />
-              </Routes>
-          </BrowserRouter>
-      </div>
-  );
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<ShowProducts />} />
+                    <Route path='/create' element={<CreateProduct />} />
+                    <Route path='/edit/:id' element={<EditProduct />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App
---
+++ src/Components/CreateProduct.jsx
@@ -0,0 +1,8 @@
+
+function CreateProduct() {
+    return (
+        <div className='App'>a</div>
+    );
+}
+
+export default CreateProduct
