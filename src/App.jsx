e79c0df424512dbbcd71dfe9ba8666c22757723e
rocha <cambia@esto.cl>
2024-12-09 23:34:25 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Importar nuestros componentes
+import ShowProducts from './Components/ShowProducts';
+import CreateProduct from './Components/CreateProduct';
+import EditProduct from './Components/CreateProduct';
+
+function App() {
+  return (
+      <div className='App'>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<ShowProducts/>}/>
+                  <Route path='/create' element={<CreateProduct/>} />
+                  <Route path='/edit/:id' element={<EditProduct/>} />
+              </Routes>
+          </BrowserRouter>
+      </div>
+  );
+}
+
+export default App
